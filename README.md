1648067e94d95645873471bb00d173dd03547dea
dragon <809629457@qq.com>
2020-06-10 16:23:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kgplayer
+基于IjkPlayer 0.8.3版本封装的基础播放器，支持http,rtsp,rtmp
---
+++ app/src/main/java/com/kuaige/kgplayer/MainActivity.java
@@ -0,0 +1,70 @@
+package com.kuaige.kgplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.kuaige.player.listener.VideoListener;
+import com.kuaige.player.player.VideoPlayer;
+
+import java.io.IOException;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+
+public class MainActivity extends AppCompatActivity implements VideoListener {
+
+    private VideoPlayer videoPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoPlayer = findViewById(R.id.video);
+
+        videoPlayer.setVideoListener(this);
+//        videoPlayer.setPath("rtmp://58.200.131.2:1935/livetv/hunantv");
+        videoPlayer.setPath("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
+//        videoPlayer.setPath("http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8");
+        try {
+            videoPlayer.load();
+        } catch (IOException e) {
+            Toast.makeText(this,"播放失败",Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBufferingUpdate(IMediaPlayer iMediaPlayer, int i) {
+
+    }
+
+    @Override
+    public void onCompletion(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public boolean onError(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(IMediaPlayer iMediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(IMediaPlayer iMediaPlayer) {
+        videoPlayer.start();
+    }
+
+    @Override
+    public void onSeekComplete(IMediaPlayer iMediaPlayer) {
+
+    }
+
+    @Override
+    public void onVideoSizeChanged(IMediaPlayer iMediaPlayer, int i, int i1, int i2, int i3) {
+
+    }
+}
